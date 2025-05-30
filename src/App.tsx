846f0113012be714f649a8ba542a5c782cbbb3b3
desalegn <desalegntamirateshetu@gmail.com>
2025-05-30 20:03:33 +0300

    implement Color Mode Switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import NavBar from "./components/NavBar";
+import ThemeToggle from "./components/ThemeToggle";
 
 export default function App() {
   return (
-    <div className="grid grid-cols-2">
-      <div className="col-span-full">
-        <NavBar />
+    <>
+      <div className="grid grid-cols-2">
+        <div className="col-span-full">
+          <NavBar />
+        </div>
+        <div className="bg-yellow-400 hidden lg:block">Aside</div>
+        <div className="bg-purple-400 col-span-full lg:col-span-1 dark:bg-purple-200">Main</div>
       </div>
-      <div className="bg-yellow-400 hidden lg:block">Aside</div>
-      <div className="bg-purple-400 col-span-full lg:col-span-1">Main</div>
-    </div>
+      <div className="fixed top-3 right-5">
+        <ThemeToggle />
+      </div>
+    </>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ export default function NavBar() {
   return (
     <div className="flex items-center">
       <img src={logo} alt="logo for game-hub" className="w-14"/>
-      <p>Navbar</p>
+      <p className="">Navbar</p>
     </div>
   )
 }
