dd7a7d21815a10e6c640f1ae4b641587a72d7777
desalegn <desalegntamirateshetu@gmail.com>
2025-05-30 18:49:59 +0300

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 export default function App() {
   return (
-    <>
-      <button className="btn btn-accent">hi</button>
-    </>
+    <div className="grid grid-cols-2">
+      <div className="bg-blue-500 col-span-full">Navbar</div>
+      <div className="bg-yellow-400 hidden lg:block">Aside</div>
+      <div className="bg-purple-400 col-span-full lg:col-span-1">Main</div>
+    </div> 
   );
 }
