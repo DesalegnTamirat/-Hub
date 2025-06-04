72e4d8b12f5b048b4c3c18a87668efb5c3a4c2b6
desalegn <desalegntamirateshetu@gmail.com>
2025-06-05 01:26:01 +0300

    Make Genres static

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,26 +1,26 @@
-import useGenres, { type Genre } from "../hooks/useGenres";
+import getGenres from "../data/genres";
+import { type Genre } from "../hooks/useGenres";
 import GenreItem from "./GenreItem";
 
 interface Props {
-  onSelectGenre: (genre: Genre) => void,
-  selectedGenre: Genre | null
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-export default function GenreList({onSelectGenre, selectedGenre}: Props) {
-  const { data: genres, error, isLoading } = useGenres();
-  if (error) return;
-  
+export default function GenreList({ onSelectGenre, selectedGenre }: Props) {
+  const genres = getGenres();
+
   return (
     <div>
-      {isLoading && (
-        <span className="loading loading-spinner loading-xl block mx-auto"></span>
-      )}
-      <div>
-        <h1 className="text-4xl font-bold pl-3">Genres</h1>
-        {genres.map((genre) => (
-          <GenreItem genre={genre} key={genre.id} onSelectGenre={onSelectGenre} selectedGenre={selectedGenre}/>
-        ))}
-      </div>
+      <h1 className="text-4xl font-bold pl-3">Genres</h1>
+      {genres.map((genre) => (
+        <GenreItem
+          genre={genre}
+          key={genre.id}
+          onSelectGenre={onSelectGenre}
+          selectedGenre={selectedGenre}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/data/genres.ts
@@ -0,0 +1,118 @@
+export default function getGenres(){
+  return [
+    {
+      id: 4,
+      name: "Action",
+      image_background:
+        "https://media.rawg.io/media/games/737/737ea5662211d2e0bbd6f5989189e4f1.jpg",
+      },
+    {
+      id: 51,
+      name: "Indie",
+      image_background:
+        "https://media.rawg.io/media/games/b54/b54598d1d5cc31899f4f0a7e3122a7b0.jpg",
+      },
+    {
+      id: 3,
+      name: "Adventure",
+      image_background:
+      "https://media.rawg.io/media/games/baf/baf9905270314e07e6850cffdb51df41.jpg",
+    },
+    {
+      id: 5,
+      name: "RPG",
+      image_background:
+      "https://media.rawg.io/media/games/d0f/d0f91fe1d92332147e5db74e207cfc7a.jpg",
+    },
+    {
+      id: 10,
+      name: "Strategy",
+      image_background:
+      "https://media.rawg.io/media/games/424/424facd40f4eb1f2794fe4b4bb28a277.jpg",
+    },
+    {
+      id: 2,
+      name: "Shooter",
+      image_background:
+      "https://media.rawg.io/media/games/73e/73eecb8909e0c39fb246f457b5d6cbbe.jpg",
+    },
+    {
+      id: 40,
+      name: "Casual",
+      image_background:
+        "https://media.rawg.io/media/games/bbf/bbf8d74ab64440ad76294cff2f4d9cfa.jpg",
+    },
+    {
+      id: 14,
+      name: "Simulation",
+      image_background:
+        "https://media.rawg.io/media/games/78d/78dfae12fb8c5b16cd78648553071e0a.jpg",
+    },
+    {
+      id: 7,
+      name: "Puzzle",
+      image_background:
+        "https://media.rawg.io/media/games/a5a/a5abaa1b5cc1567b026fa3aa9fbd828e.jpg",
+    },
+    {
+      id: 11,
+      name: "Arcade",
+      image_background:
+        "https://media.rawg.io/media/games/363/36306deef81e7955a5d0f5c3b43fccee.jpg",
+    },
+    {
+      id: 83,
+      name: "Platformer",
+      image_background:
+        "https://media.rawg.io/media/games/cd3/cd3c9c7d3e95cb1608fd6250f1b90b7a.jpg",
+    },
+    {
+      id: 59,
+      name: "Massively Multiplayer",
+      image_background:
+        "https://media.rawg.io/media/games/b69/b69a67833630dd96d8eee9d2c8c27574.jpg",
+      },
+      {
+        id: 1,
+        name: "Racing",
+        image_background:
+        "https://media.rawg.io/media/games/370/3703c683968a54f09630dcf03366ea35.jpg",
+      },
+      {
+        id: 15,
+        name: "Sports",
+        image_background:
+        "https://media.rawg.io/media/screenshots/376/376c0927f2ff4c5fd38a95d5ff501bba.jpg",
+      },
+      {
+        id: 6,
+        name: "Fighting",
+        image_background:
+        "https://media.rawg.io/media/screenshots/5a1/5a1ce6908dc401fa9f805d5db4e832d7.jpg",
+      },
+      {
+      id: 19,
+      name: "Family",
+      image_background:
+      "https://media.rawg.io/media/screenshots/a8c/a8cd58ee985ab6d509370bddada91dbd.jpeg",
+    },
+    {
+      id: 28,
+      name: "Board Games",
+      image_background:
+        "https://media.rawg.io/media/screenshots/bfc/bfcdbc0a64ce19da4ce4cac8b7d2d985.jpg",
+    },
+    {
+      id: 17,
+      name: "Card",
+      image_background:
+        "https://media.rawg.io/media/screenshots/fbd/fbdca928fdd7439cba68443cf4d8c6c1.jpg",
+    },
+    {
+      id: 34,
+      name: "Educational",
+      image_background:
+      "https://media.rawg.io/media/screenshots/096/09627345aa95f0636ea89d326e6ea17c.jpg",
+    },
+  ];
+}
